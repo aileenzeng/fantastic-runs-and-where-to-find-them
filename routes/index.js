4589ce6f1bb73327b7e76520e656fa13f2cd7da4
Aileen Zeng <aileenz@cs.washington.edu>
2018-10-20 22:06:49 -0700

    Hooked up index page to app

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 const routesController = require('../controllers').routes;
 
 /* Home page */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'), { title: 'Express' });
 });
 
 /* Routes */
